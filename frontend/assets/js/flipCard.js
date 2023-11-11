1f07e945803d4bb38a3f0823f097bbabdb875ba8
Ilana Lipkina <ilanalipkina@gmail.com>
2023-11-11 23:30:16 +0400

    feat: add checking pairs

--- frontend/assets/js/flipCard.js
+++ frontend/assets/js/flipCard.js
@@ -1,12 +1,34 @@
 let cardTransitionTime = 500;
 let cards = document.querySelectorAll(".card");
+let cardsWrapper = document.querySelector(".cards-wrapper")
 let switching = false;
 
+
 function flipCardListener() {
-    cards = document.querySelectorAll(".card")
+    cards = document.querySelectorAll(".card");
+    let lastSeenCard = null
+
     cards.forEach((card) => card.addEventListener("click", flipCard = () => {
-        console.log('clicked');
-        card.classList.toggle("is-flipped");
+
+        card.classList.add("is-flipped");
+
+        if (lastSeenCard) {
+            if (lastSeenCard.children[1].children[0].src === card.children[1].children[0].src) {
+                lastSeenCard = null;
+                return;
+            } else {
+                cardsWrapper.style.pointerEvents = 'none';
+
+                setTimeout(() => {
+                    lastSeenCard.classList.remove('is-flipped');
+                    card.classList.remove('is-flipped');
+                    lastSeenCard = null;
+                    cardsWrapper.style.pointerEvents = '';
+                }, 400)
+                return;
+            }
+        }
+        lastSeenCard = card
     }));
 }
 
